0461d1733f87fd29a55510bea128f079a9da53a2
Jordan Pitters <jordan_pitters@hotmail.com>
2019-02-09 14:51:45 -0500

    Just a random idea

---
+++ OpenGL_2D_Tanks/SceneNode.h
@@ -0,0 +1,38 @@
+#ifndef SCENENODE_HEADER
+#define SCENENODE_HEADER
+
+
+#include "glm\glm.hpp"
+#include <glm\gtx\matrix_decompose.hpp>
+#include "glm\gtc\matrix_transform.hpp"
+#include <vector>
+#include "GL\glut.h"
+
+class SceneNode
+{
+	protected:
+	std::vector<SceneNode*> children;
+	SceneNode* parent;
+	glm::mat4 transformation;
+	float r, g, b;
+	float scale;
+
+
+	public:
+		//Constructor
+		SceneNode(glm::mat4 transformation, float scale);
+
+		//Methods
+		void SceneNode::addChild(SceneNode*);
+		void SceneNode::setColor(float r, float g, float b);
+		glm::mat4 SceneNode::getTransformationMatrix();
+		std::vector<SceneNode*> getChildren();
+		SceneNode* SceneNode::getParent();
+		void SceneNode::setParent(SceneNode* p);
+		void SceneNode::render();
+		virtual void SceneNode::draw(float scale) = 0;
+
+		virtual void SceneNode::tick();
+		void SceneNode::setTransformationMatrix(glm::mat4);
+};
+#endif // !SCENENODE_HEADER
